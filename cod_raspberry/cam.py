4e0523dd40315a7f13c24505b6b1e5b5284eafd9
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2018-05-04 18:52:35 -0300

    Add files via upload

---
+++ cod_raspberry/cam.py
@@ -0,0 +1,33 @@
+import urllib2
+import cv2
+import numpy as np
+import time
+import requests
+import os
+
+# Replace the URL with your own IPwebcam shot.jpg IP:port
+url='http://***.**.**.***:8080/shot.jpg'
+
+while True:
+	# Use urllib to get the image from the IP camera
+	imgResp = urllib2.urlopen(url)
+
+	# Numpy to convert into a array
+	imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
+
+	# Finally decode the array to OpenCV usable format ;) 
+	img = cv2.imdecode(imgNp,-1)
+
+	cv2.imwrite('foto.png',img)
+	
+	#To give the processor some less stress
+	time.sleep(1) 
+
+	os.system("sshpass -p '********' scp foto.png reynaldo@ime.usp.br:www/phd/internet_cosas/")
+
+	time.sleep(1)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+
